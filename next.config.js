1280e42c94bf45c2bbe9c57192158d0815ca0357
Aviel Naftali <68643310+orangdong@users.noreply.github.com>
2023-03-20 22:05:04 +0700

    update image config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      protocol: 'https',
-      hostname: 'storage.googleapis.com',
-      port: '',
-      pathname: '/proglabs-bucket/course-thumbnail/**',
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/proglabs-bucket/course-thumbnail/**",
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
