72fa6615d6594449cdf6294dffdd2c694bd81c77
Aviel Naftali <68643310+orangdong@users.noreply.github.com>
2023-03-20 21:59:18 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     remotePatterns: [
       protocol: 'https',
       hostname: 'storage.googleapis.com',
+      port: '',
       pathname: '/proglabs-bucket/course-thumbnail/**',
     ],
   },
