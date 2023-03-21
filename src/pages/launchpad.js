85967614b05126e71f4ddce5eef3368dcae53121
Aviel Naftali <dev.naftaliaviel@gmail.com>
2023-03-21 20:07:46 +0700

    update launchpad

--- src/pages/launchpad.js
+++ src/pages/launchpad.js
@@ -1,5 +1,72 @@
 import GeneralLayout from "@/components/GeneralLayout";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { fetchData } from "@/lib/fetchData";
+import Image from "next/image";
 
-export default function Launchpad() {
-  return <GeneralLayout title={"Launchpad"}></GeneralLayout>;
+export default function Launchpad({ courses }) {
+  return (
+    <GeneralLayout title={"Launchpad"} courses={courses}>
+      <Box>
+        <Flex
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          h={"full"}
+          maxW={"1280px"}
+          w={"full"}
+          mx={"auto"}
+          px={4}
+        >
+          <Flex
+            my={{ base: "60px", md: "92px" }}
+            flexDir={"column"}
+            alignItems={"center"}
+            w={"full"}
+          >
+            <Text
+              textAlign={"center"}
+              fontWeight={"bold"}
+              fontSize={{ base: 36, md: 48 }}
+              mb={4}
+            >
+              Launchpad
+            </Text>
+            <Text
+              textAlign={"center"}
+              fontSize={{ base: "18px", md: "22px" }}
+              color={"textGray"}
+              mb={"60px"}
+            >
+              Get your NFT membership and start learning
+            </Text>
+            <Flex justifyContent={"space-between"} alignItems={"center"}>
+              <Image
+                src="/assets/launchpad.png"
+                width="500"
+                height="500"
+                alt="launchpad"
+                style={{ borderRadius: "24px" }}
+              />
+              <Box
+                borderRadius={"24px"}
+                p={6}
+                bg={"backgroundDark"}
+                maxW={"500px"}
+                maxH={"500px"}
+              >
+                <Text>Progpass</Text>
+              </Box>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Box>
+    </GeneralLayout>
+  );
 }
+
+export const getServerSideProps = async () => {
+  const data = await fetchData("/v1/courses");
+
+  return {
+    props: { courses: data },
+  };
+};
