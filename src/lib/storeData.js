0cef754708e90703d0c65008dd88bff2500d0039
Aviel Naftali <dev.naftaliaviel@gmail.com>
2023-03-29 10:33:38 +0700

    update fetch & store data

--- src/lib/storeData.js
+++ src/lib/storeData.js
@@ -1,19 +1,21 @@
-export const fetchData = async (endpoint) => {
+export const storeData = async ({ endpoint, body, method }) => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
     headers: {
       "Content-Type": "application/json",
       "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
     },
+    body: JSON.stringify(body),
+    method,
   })
     .then((response) => response.json())
-    .then((data) => data);
+    .then((data) => data)
+    .catch(console.error);
 
-  if (data.status === "error") {
+  if (data?.status === "error") {
     return {
       status: data.status,
       message: data.message,
     };
   }
-
-  return data.data;
+  return data?.data;
 };
