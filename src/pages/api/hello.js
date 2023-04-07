3028503ed06d9ac14961a53539cdda870eed2f13
Aviel Naftali <dev.naftaliaviel@gmail.com>
2023-04-07 11:34:55 +0700

    Update dashboard

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getToken } from "next-auth/jwt";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  const secret = process.env.JWT_SECRET;
+  const token = await getToken({ req, secret });
+  console.log("JSON Web Token", token);
+
+  res.status(200).json({ name: "John Doe", token });
 }
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,8 +1,7 @@
 import DashboardLayout from "@/components/DashboardLayout";
-import { useSession } from "next-auth/react";
 
 export default function Dashboard() {
-  const { status, data } = useSession();
-  console.log(status, data);
-  return <DashboardLayout title={"Dashboard"}>Dashboard</DashboardLayout>;
+  return (
+    <DashboardLayout title={"Dashboard"}>This is Dashboard</DashboardLayout>
+  );
 }
