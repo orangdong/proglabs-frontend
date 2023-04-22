8707c7a61faca131937fbd94b7b7ca07f7261e36
Aviel Naftali <dev.naftaliaviel@gmail.com>
2023-04-22 13:32:54 +0700

    finish my course detail

--- src/pages/dashboard/my-courses/[id].js
+++ src/pages/dashboard/my-courses/[id].js
@@ -2,6 +2,21 @@ import DashboardLayout from "@/components/DashboardLayout";
 import useMembership from "@/lib/useMembership";
 import { getSession } from "next-auth/react";
 import { fetchData } from "@/lib/fetchData";
+import {
+  Box,
+  Button,
+  Flex,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { FiCheckCircle } from "react-icons/fi";
+import { useState } from "react";
+import useToastHook from "@/components/Atoms/ToastHook";
+import { storeData } from "@/lib/storeData";
 
 export default function MyCoursesDetail({ memberships, course, session }) {
   const isMembership = useMembership({
@@ -9,10 +24,148 @@ export default function MyCoursesDetail({ memberships, course, session }) {
     redirect: true,
     isPremium: course.isPremium,
   });
+  const [toast, setToast] = useToastHook();
+  const [allLessons, setAllLessons] = useState(
+    [].concat(
+      ...course.courseModules.map((cm) => cm.courseLessons.map((cl) => cl))
+    )
+  );
+
+  const handleUpdate = async ({ lessonId, e }) => {
+    e.preventDefault();
+
+    const model = {
+      lessonId,
+    };
+
+    const finishedLesson = allLessons.filter((al) => al.isFinished).length;
+    const totalLesson = allLessons.length;
+
+    if (totalLesson === finishedLesson + 1) {
+      const currDate = new Date();
+      const options = { month: "short", day: "numeric", year: "numeric" };
+      const date = currDate.toLocaleDateString("en-US", options);
+      const isCourseFinished = {
+        isComplete: true,
+        id: course.id,
+        date,
+      };
+
+      model.isCourseFinished = isCourseFinished;
+    }
+
+    const submit = await storeData({
+      body: model,
+      endpoint: `/v1/courses/lesson`,
+      headers: {
+        "x-auth-token": session.accessToken,
+      },
+      method: "PUT",
+    });
+
+    // set state disable
+    const newItem = [...allLessons];
+    const findIndex = newItem.findIndex((ni) => ni.id === lessonId);
+    newItem[findIndex].isFinished = true;
+    setAllLessons(newItem);
+
+    return setToast({
+      type: "success",
+      message: "Course lesson finished",
+    });
+  };
 
   return (
     <DashboardLayout title={"My Course Detail"}>
-      MyCoursesDetail
+      <Tabs
+        variant="unstyled"
+        display={"flex"}
+        flexDir={{ base: "column-reverse", md: "row-reverse" }}
+        position="relative"
+        justifyContent={{ base: "normal", md: "space-between" }}
+      >
+        <TabList
+          display={"flex"}
+          flexDir={"column"}
+          w={"full"}
+          maxW={{ base: "100%", md: "30%" }}
+          maxH={"550px"}
+          overflowY={"auto"}
+        >
+          <Text fontWeight={"semiBold"} fontSize={"22px"} mb={5}>
+            Course modules
+          </Text>
+          {course.courseModules.map((cm, i) => (
+            <>
+              <Text fontSize={"18px"} fontWeight={"semiBold"} mb={2}>
+                {cm.title}
+              </Text>
+              {cm.courseLessons.map((cl, i) => (
+                <Tab
+                  _selected={{
+                    background: "#293F48",
+                    color: "white",
+                  }}
+                  fontSize={"18px"}
+                  background={"gray.100"}
+                  borderRadius={"12px"}
+                  fontWeight={"medium"}
+                  py={4}
+                  mb={3}
+                  key={i}
+                >
+                  {cl.title}
+                </Tab>
+              ))}
+            </>
+          ))}
+        </TabList>
+        <TabPanels
+          w={"full"}
+          maxW={{ base: "100%", md: "65%" }}
+          maxH={"550px"}
+          overflowY={"auto"}
+        >
+          {allLessons.map((al, i) => (
+            <TabPanel key={i}>
+              <Flex w={"full"} flexDir={"column"}>
+                <Flex
+                  w={"full"}
+                  mb={5}
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                  flexWrap={"wrap"}
+                >
+                  <Text fontWeight={"semiBold"} fontSize={"22px"}>
+                    Course content
+                  </Text>
+                  <Button
+                    bg={"backgroundTeal"}
+                    color={"white"}
+                    borderRadius={"12px"}
+                    _hover={{
+                      bg: "backgroundTealHover",
+                    }}
+                    isDisabled={al.isFinished}
+                    leftIcon={al.isFinished ? <FiCheckCircle /> : null}
+                    onClick={(e) => handleUpdate({ lessonId: al.id, e })}
+                    w={{ base: "full", md: "fit-content" }}
+                    mt={{ base: 2, md: 0 }}
+                  >
+                    {al.isFinished ? "Finished" : "Mark as finished"}
+                  </Button>
+                </Flex>
+                <Box>
+                  <Text fontWeight={"medium"} fontSize={"18px"} mb={3}>
+                    {al.title}
+                  </Text>
+                  <p>{al.content}</p>
+                </Box>
+              </Flex>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
     </DashboardLayout>
   );
 }
@@ -46,7 +199,9 @@ export const getServerSideProps = async ({ req, params }) => {
   const memberships = await fetchData("/v1/membership", {
     "x-auth-token": session.accessToken,
   });
-  const course = await fetchData(`/v1/courses/${id}`);
+  const course = await fetchData(`/v1/me/my-course/${id}?includeLessons=true`, {
+    "x-auth-token": session.accessToken,
+  });
 
   return {
     props: {
